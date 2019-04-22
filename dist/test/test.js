fbc07c75d7ec575d207bc69687a6610163dba549
Justin Mahar <justin.m.mahar@gmail.com>
2019-04-22 17:10:53 -0500

    Add tests

---
+++ dist/test/test.js
@@ -0,0 +1,95 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var index_1 = __importDefault(require("../index"));
+var chai_1 = __importDefault(require("chai"));
+var expect = chai_1.default.expect;
+describe("Chooser function test", function () {
+    it("should choose a weighted index", function () {
+        var arrayOfWeights = [10, 50, 45, 5];
+        var index = index_1.default.chooseWeightedIndex(arrayOfWeights);
+        expect(index)
+            .to.be.gte(0)
+            .and.lte(4);
+    });
+    it("should choose an object based on weight properties", function () {
+        var iceCreamFlavors = [
+            { flavor: "chocolate", weight: 3 },
+            { flavor: "vanilla", weight: 1 },
+            { flavor: "poop", weight: 0 },
+            { flavor: "piña colada", weight: 6 }
+        ];
+        var item = index_1.default.chooseWeightedObject(iceCreamFlavors);
+        expect(["chocolate", "vanilla", "poop", "piña colada"]).to.include(item.flavor);
+    });
+    it("should choose an object based on custom weight properties", function () {
+        var restaurantRatings = [
+            { name: "Chipotle", rating: 4.2 },
+            { name: "Moe's", rating: 4.9 },
+            { name: "Edgy Burrito", rating: 3.4 }
+        ];
+        var item = index_1.default.chooseWeightedObject(restaurantRatings, "rating");
+        expect(["Chipotle", "Moe's", "Edgy Burrito"]).to.include(item.name);
+    });
+    it("should handle missing custom weight properties", function () {
+        var restaurantRatings = [
+            { name: "Chipotle", rating: 4.2 },
+            { name: "Moe's", rating: 4.9 },
+            { name: "Edgy Burrito" }
+        ];
+        var item = index_1.default.chooseWeightedObject(restaurantRatings, "rating");
+        expect(["Chipotle", "Moe's", "Edgy Burrito"]).to.include(item.name);
+    });
+    it("should use a default weight property when missing", function () {
+        var restaurantRatings = [
+            { name: "Chipotle", rating: 0.00000001 },
+            { name: "Moe's", rating: 0.00000001 },
+            { name: "Edgy Burrito" }
+        ];
+        var item = index_1.default.chooseWeightedObject(restaurantRatings, "rating", 100000000000);
+        expect("Edgy Burrito").to.equal(item.name);
+    });
+    it("should use a default weight when no weights are specified", function () {
+        var restaurants = [
+            { name: "Chipotle" },
+            { name: "Moe's" },
+            { name: "Edgy Burrito" }
+        ];
+        var item = index_1.default.chooseWeightedObject(restaurants);
+        expect(["Chipotle", "Moe's", "Edgy Burrito"]).to.include(item.name);
+    });
+    it("should return null when choosing weighted object from an empty list", function () {
+        var restaurants = [];
+        var val = index_1.default.chooseWeightedObject(restaurants);
+        expect(val).to.be.null;
+    });
+    it("should return null when choosing weighted object from a null value", function () {
+        var restaurants = null;
+        var val = index_1.default.chooseWeightedObject(restaurants);
+        expect(val).to.be.null;
+    });
+    it("should return null when choosing weighted object from a non-array", function () {
+        expect(index_1.default.chooseWeightedObject({ "mykey": "someval" })).to.be.null;
+        expect(index_1.default.chooseWeightedObject("blah")).to.be.null;
+        expect(index_1.default.chooseWeightedObject(12345)).to.be.null;
+        expect(index_1.default.chooseWeightedObject(3.14159)).to.be.null;
+    });
+    it("should return -1 when choosing weighted index from an empty list", function () {
+        var restaurants = [];
+        var val = index_1.default.chooseWeightedIndex(restaurants);
+        expect(val).to.equal(-1);
+    });
+    it("should return -1 when choosing weighted index from null value", function () {
+        var restaurants = null;
+        var val = index_1.default.chooseWeightedIndex(null);
+        expect(val).to.equal(-1);
+    });
+    it("should return -1 when choosing weighted index from a non-array", function () {
+        expect(index_1.default.chooseWeightedIndex({ "mykey": "someval" })).to.equal(-1);
+        expect(index_1.default.chooseWeightedIndex("blah")).to.equal(-1);
+        expect(index_1.default.chooseWeightedIndex(12345)).to.equal(-1);
+        expect(index_1.default.chooseWeightedIndex(3.14159)).to.equal(-1);
+    });
+});
---
+++ lib/test/test.ts
@@ -0,0 +1,109 @@
+import Chooser from "../index";
+import chai from "chai";
+
+let expect = chai.expect;
+
+describe("Chooser function test", () => {
+  it("should choose a weighted index", () => {
+    let arrayOfWeights = [10, 50, 45, 5];
+    let index = Chooser.chooseWeightedIndex(arrayOfWeights);
+    expect(index)
+      .to.be.gte(0)
+      .and.lte(4);
+  });
+
+  it("should choose an object based on weight properties", () => {
+    let iceCreamFlavors = [
+      { flavor: "chocolate", weight: 3 },
+      { flavor: "vanilla", weight: 1 },
+      { flavor: "poop", weight: 0 },
+      { flavor: "piña colada", weight: 6 }
+    ];
+    let item: any = Chooser.chooseWeightedObject(iceCreamFlavors);
+    expect(["chocolate", "vanilla", "poop", "piña colada"]).to.include(
+      item.flavor
+    );
+  });
+
+  it("should choose an object based on custom weight properties", () => {
+    let restaurantRatings = [
+      { name: "Chipotle", rating: 4.2 },
+      { name: "Moe's", rating: 4.9 },
+      { name: "Edgy Burrito", rating: 3.4 }
+    ];
+    let item: any = Chooser.chooseWeightedObject(restaurantRatings, "rating");
+    expect(["Chipotle", "Moe's", "Edgy Burrito"]).to.include(item.name);
+  });
+
+  it("should handle missing custom weight properties", () => {
+    let restaurantRatings = [
+      { name: "Chipotle", rating: 4.2 },
+      { name: "Moe's", rating: 4.9 },
+      { name: "Edgy Burrito" }
+    ];
+    let item: any = Chooser.chooseWeightedObject(restaurantRatings, "rating");
+    expect(["Chipotle", "Moe's", "Edgy Burrito"]).to.include(item.name);
+  });
+
+  it("should use a default weight property when missing", () => {
+    let restaurantRatings = [
+      { name: "Chipotle", rating: 0.00000001 },
+      { name: "Moe's", rating: 0.00000001 },
+      { name: "Edgy Burrito" }
+    ];
+    let item: any = Chooser.chooseWeightedObject(
+      restaurantRatings,
+      "rating",
+      100000000000
+    );
+    expect("Edgy Burrito").to.equal(item.name);
+  });
+
+  it("should use a default weight when no weights are specified", () => {
+    let restaurants = [
+      { name: "Chipotle" },
+      { name: "Moe's" },
+      { name: "Edgy Burrito" }
+    ];
+    let item: any = Chooser.chooseWeightedObject(restaurants);
+    expect(["Chipotle", "Moe's", "Edgy Burrito"]).to.include(item.name);
+  });
+  
+  it("should return null when choosing weighted object from an empty list", () => {
+    let restaurants:any = [];
+    let val: any = Chooser.chooseWeightedObject(restaurants);
+    expect(val).to.be.null
+  });
+  
+  it("should return null when choosing weighted object from a null value", () => {
+    let restaurants:any = null;
+    let val: any = Chooser.chooseWeightedObject(restaurants);
+    expect(val).to.be.null
+  });
+  
+  it("should return null when choosing weighted object from a non-array", () => {
+    expect(Chooser.chooseWeightedObject({"mykey":"someval"})).to.be.null;
+    expect(Chooser.chooseWeightedObject("blah")).to.be.null;
+    expect(Chooser.chooseWeightedObject(12345)).to.be.null;
+    expect(Chooser.chooseWeightedObject(3.14159)).to.be.null;
+  });
+  
+  it("should return -1 when choosing weighted index from an empty list", () => {
+    let restaurants:any = [];
+    let val: any = Chooser.chooseWeightedIndex(restaurants);
+    expect(val).to.equal(-1)
+  });
+  
+  it("should return -1 when choosing weighted index from null value", () => {
+    let restaurants:any = null;
+    let val: any = Chooser.chooseWeightedIndex(null);
+    expect(val).to.equal(-1)
+  });
+  
+  it("should return -1 when choosing weighted index from a non-array", () => {
+    expect(Chooser.chooseWeightedIndex({"mykey":"someval"})).to.equal(-1);
+    expect(Chooser.chooseWeightedIndex("blah")).to.equal(-1);
+    expect(Chooser.chooseWeightedIndex(12345)).to.equal(-1);
+    expect(Chooser.chooseWeightedIndex(3.14159)).to.equal(-1);
+  });
+});
